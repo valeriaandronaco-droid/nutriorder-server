5f8dceffbf4deef634bfe1ec222ea4b6a44c50ce
valeriaandronaco-droid <valeria.andronaco@gmail.com>
2026-03-07 21:44:35 +0100

    aggiungo log debug

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const mysql = require("mysql2/promise");
 
+console.log("DB Config:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "nutriorder",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
 });
